beba1f23b84ac3738de11db36f1cf5f5aa64167d
Vidya Maruti Kore <vidya.maruti.kore@sap.com>
2023-05-22 10:31:10 +0530

    committed maven project

---
+++ src/main/java/com/devops/project/maven_devops_project/App.java
@@ -0,0 +1,13 @@
+package com.devops.project.maven_devops_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
